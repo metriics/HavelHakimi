ec602ff68f502b786b4579bd183aadd72ae1655e
Steph <stephane.gagnon00@gmail.com>
2021-04-25 12:09:11 -0400

    Add project files.

---
+++ HavelHakimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HavelHakimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
