803b5b32d3a904d38ca26aa095292529502b47f3
Steph <stephane.gagnon00@gmail.com>
2021-04-28 17:57:50 -0400

    done but wrong? some sort of issue for test sequence 4 and 5

--- HavelHakimi/Program.cs
+++ HavelHakimi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HavelHakimi
 {
@@ -6,7 +7,57 @@ namespace HavelHakimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[][] sequences =
+            {
+                new int[]{ 5, 3, 0, 2, 6, 2, 0, 7, 2, 5 },
+                new int[]{ 4, 2, 0, 1, 5, 0 },
+                new int[]{ 3, 1, 2, 3, 1, 0 },
+                new int[]{ 16, 9, 9, 15, 9, 7, 9, 11, 17, 11, 4, 9, 12, 14, 14, 12, 17, 0, 3, 16 }, // this breaks the code
+                new int[]{ 14, 10, 17, 13, 4, 8, 6, 7, 13, 13, 17, 18, 8, 17, 2, 14, 6, 4, 7, 12 }, // this breaks the code
+                new int[]{ 15, 18, 6, 13, 12, 4, 4, 14, 1, 6, 18, 2, 6, 16, 0, 9, 10, 7, 12, 3 },
+                new int[]{ 6, 0, 10, 10, 10, 5, 8, 3, 0, 14, 16, 2, 13, 1, 2, 13, 6, 15, 5, 1 },
+                new int[]{ 2, 2, 0 },
+                new int[]{ 3, 2, 1 },
+                new int[]{ 1, 1 },
+                new int[]{ 1 },
+                new int[]{ }
+            };
+
+            for (int i = 0; i < sequences.Length; i++)
+            {
+                Sequence currentSequence = new Sequence(sequences[i]);
+                Console.WriteLine(currentSequence.ToString() + $" => {HavelHakimi(currentSequence)}");
+            }
+        }
+
+        public static bool HavelHakimi(Sequence seq)
+        {
+            // Havel-Hakimi algorithm
+            seq.RemoveZeroes();
+
+            // make sure the sequence is not empty
+            if (seq.Get().Length == 0)
+            {
+                return true;
+            }
+
+            // sort the sequence in descending order
+            seq.SortDescending();
+
+            // remove first answer from seqeunce
+            int n = seq.RemoveFirst();
+
+            // make sure n is not greater than the new length of the sequence
+            if (seq.LengthCheck(n))
+            {
+                return false;
+            }
+
+            // subtract 1 from the first n elements
+            seq.FrontElimination(n);
+
+            // recursion! :0 
+            return HavelHakimi(seq);
         }
     }
 }
---
+++ HavelHakimi/Sequence.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HavelHakimi
+{
+    class Sequence
+    {
+        private List<int> seq;
+
+        // State bools
+        private bool isDescending = false;
+
+        public Sequence(int[] newseq)
+        {
+            seq = new List<int>(newseq);
+        }
+
+        public override string ToString()
+        {
+            string message = "[";
+
+            foreach (int answer in seq)
+            {
+                message += answer.ToString() + ", ";
+            }
+
+            if (seq.Count == 0)
+            {
+                return message + "]";
+            }
+            else
+            {
+                // remove the trailing ', ' and return
+                return message.Substring(0, message.Length - 2) + "]";
+            }
+        }
+
+        /// <summary>
+        /// Get the sequence as an array.
+        /// </summary>
+        /// <returns>int[] seq</returns>
+        public int[] Get()
+        {
+            return seq.ToArray();
+        }
+
+        /// <summary>
+        /// Loops until the sequence no longer contains any instances of 0.
+        /// </summary>
+        public void RemoveZeroes()
+        {
+            bool containsZero = true;
+
+            while (containsZero)
+            {
+                containsZero = seq.Remove(0);
+            }
+        }
+
+        /// <summary>
+        /// Sorts the sequence in descending order.
+        /// </summary>
+        public void SortDescending()
+        {
+            if (!isDescending)
+            {
+                DescendingSorter ds = new DescendingSorter();
+                seq.Sort(ds);
+                isDescending = true;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given number n is greater than the size of the sequence.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>true if n is greater than the size of the sequence, and false if n is less than or equal to the size of the sequence.</returns>
+        public bool LengthCheck(int n)
+        {
+            if (n > seq.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Subtracts 1 from the first n answers in the sequence. Assumes n is greater than 0 and no greater than the size of the sequence.
+        /// </summary>
+        /// <param name="n"></param>
+        public void FrontElimination(int n)
+        {
+            if (!isDescending)
+            {
+                SortDescending();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                seq[i]--;
+            }
+        }
+
+        public int RemoveFirst()
+        {
+            int first = seq[0];
+            seq.RemoveAt(0);
+            return first;
+        }
+    }
+
+    class DescendingSorter : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == 0 || y == 0)
+            {
+                return 0;
+            }
+
+            return y.CompareTo(x);
+        }
+    }
+}
